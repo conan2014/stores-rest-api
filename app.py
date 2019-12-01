b422b3ed327d4c9e333c896f42993b660e891e10
Xu Huang <xu.huang2019@gmail.com>
2019-12-01 16:11:45 -0500

    First commit of app.py file from REST API with Flask and Python udemy course

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from flask_restful import Api
+
+from flask_jwt import JWT
+from security import authenticate, identity  # Flask-JWT requires these 2 functions to know how to handle an incoming JWT
+                                             # and also what data we want to store in an outgoing JWT
+
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # turn off flask-sqlalchemy modification tracker to free up resources
+                                                     # because sqlalchemy has its own modification tracker
+                                                     # this does not turn off sqlalchemy modification tracker
+app.secret_key = 'jose' # app.secret_key is used to encode the JWT so you know it's your app that created it, not anyone else
+                        # so that an end user cannot modify it in their browser
+                        # Make this long, random, and secret in a real app
+api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()   # SQLAlchemy only creates tables that it sees according to import
+                      # So if we delete 'from resources.store import Store, StoreList', then SQLAlchemy won't create stores table
+
+jwt = JWT(app, authenticate, identity) # as soon as we create a JWT object,
+                                       # Flask-JWT registers an endpoint with our application, /auth
+
+api.add_resource(UserRegister, '/register')
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
+
+if __name__ == "__main__":
+    from db import db
+    db.init_app(app)
+    app.run(debug=True, port=4996)
